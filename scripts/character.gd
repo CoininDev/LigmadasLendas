066336f6caa1b020ee132cd26b25eb3d14538861
CoininDev <gabr.batz@gmail.com>
2024-01-13 18:20:05 -0300

    movimentação básica

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,10 +1,18 @@
 extends CharacterBody3D
-
+@export var speed = 500
 @onready var navagent = $NavigationAgent3D
 
-
+#funções centrais
+func _process(delta):
+	if navagent.is_navigation_finished():
+		return
+	move(delta)
 func _input(event):
-	if Input.is_action_just_pressed("mright"):
+	select_target()
+#funções específicas
+func select_target():
+	
+	if Input.is_action_pressed("mright"):
 		#converter o ponto do mouse na tela 2d para um ponto 3d no jogo.
 		#para isso é preciso criar um raio, uma linha reta que parte da camera e segue até encontrar uma colisão.
 		var camera = get_tree().get_nodes_in_group("camera")[0]
@@ -17,5 +25,11 @@ func _input(event):
 		rayquery.from = from
 		rayquery.to = to
 		var result = space.intersect_ray(rayquery)
-		#mover até o ponto encontrado
 		print(result)
+		if result.has("position"):
+			navagent.target_position = result.position
+func move(delta):
+	var targetpos = navagent.get_next_path_position()
+	var dir = global_position.direction_to(targetpos)
+	velocity = dir * speed * delta
+	move_and_slide()
