b467a8b643cdd31a56ed1a611be1ee27cc7f1393
CoininDev <gabr.batz@gmail.com>
2024-02-16 23:14:34 -0300

    state machine incompleto

---
+++ comps/scripts/StateMachineComponent.gd
@@ -0,0 +1,34 @@
+extends Node
+@export var initial_state:State
+var current_state:State
+var states:Dictionary = {}
+
+
+func _ready():
+	for child in get_children():
+		if child is State:
+			states[child.name.to_lower()] = child
+	if initial_state:
+		initial_state.enter()
+		current_state = initial_state
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if current_state:
+		current_state.physics_update(delta)
+		current_state.transitioned.connect(on_child_transition)
+
+func on_child_transition(state:State, new_state_name:String):
+	if state != current_state:
+		return
+	
+	var new_state:State = states.get(new_state_name.to_lower())
+	if not new_state:
+		return
+	
+	if current_state:
+		current_state.exit()
+	
+	new_state.enter()
+	current_state = new_state
---
+++ scripts/State.gd
@@ -0,0 +1,22 @@
+extends Node
+class_name State
+#pra que serve um State?
+#Serve para definir estados de uma unidade.
+#Por exemplo, a IA de um creep vai ter estados:
+	#1: seguir o núcleo inimigo,
+	#2: se tiver uma unidade inimiga perto(torre, herói ou creep), ele anda até chegar mais perto
+	#3: quando ele estar numa distância interessante dela, ele ataca.
+#o State faz parte de uma estrutura que faz esse tipo de programação funcionar.
+#é basicamente no que ele vai se basear para mudar seu comportamento diante das condições.
+
+signal transitioned 
+
+func enter():
+	pass
+func exit():
+	pass
+func update(_delta:float):
+	pass
+func physics_update(_delta:float):
+	pass
+	
